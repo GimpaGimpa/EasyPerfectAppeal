b580b45432ecaf57389d9cb51d80e58b9aa50149
GimpaGimap <android.sugoi@gmail.com>
2020-01-09 17:32:37 +0900

    メンタル値入力と閾値表示機能を追加 表示を調整

--- EasyPerfectAppeal.py
+++ EasyPerfectAppeal.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('/usr/local/lib/python3.8/site-packages')
 import pyautogui
 import ctypes
+
 #タイミング定数
 MENTAL_100 = 1
 MENTAL_90 = 0.9
@@ -11,30 +12,61 @@ MENTAL_70 = 0.7
 MENTAL_50 = 0.5
 MENTAL_20 = 0.3
 
+#アピールバー閾値定数
+THRESHOLD_1 = 0.91
+THRESHOLD_2 = 0.71
+THRESHOLD_3 = 0.51
+THRESHOLD_4 = 0.1
+
+#自動クリック関数
 def perfect_click(timing):
     pyautogui.click()
     time.sleep(timing)
     pyautogui.click()
 
-print('\n攻撃対象にカーソル合わせてキーを押してね\n')
-print('1    .. max')
-print('2    .. 90%↓')
-print('3    .. 70%↓')
-print('4    .. 50%↓')
-print('5    .. 10%↓')
-print('esc  .. 終了')
-print('\n動作中........')
+    #メンタル値入力関数
+def mental_input():
+    mental_maximum = input('メンタル値 -> ')
+    mental_maximum = float(mental_maximum)
+    mental = {'above91' : int(mental_maximum * THRESHOLD_1),\
+                'above71' : int(mental_maximum * THRESHOLD_2),\
+                'above51' : int(mental_maximum * THRESHOLD_3),\
+                'above10' : int(mental_maximum * THRESHOLD_4)}
+    display_message(mental)
+
+    #メッセージ表示関数
+def display_message(mental):
+    print('\n*****************************************')
+    print('\n攻撃対象にカーソル合わせてキーを押してね\n')
+    print('Alt + 1  ..  %d↑'%mental['above91'])
+    print('Alt + 2  ..  %d↑'%mental['above71'])
+    print('Alt + 3  ..  %d↑'%mental['above51'])
+    print('Alt + 4  ..  %d↑'%mental['above10'])
+    print('Alt + 5  ..  ~0\n')
+    print('Alt + Q  ..  メンタル再入力')
+    print('esc      ..  終了')
+    print('\n動作中........')
+
+################################################################
+################################################################
+################################################################
+
+mental_input()
 
 while True:
-    if(ctypes.windll.user32.GetAsyncKeyState(0x31)&0x8000):
+    #0x12..Altキー 0x31~..12345 0x51..Q
+    if(ctypes.windll.user32.GetAsyncKeyState(0x31)&ctypes.windll.user32.GetAsyncKeyState(0x12)&0x8000):
         perfect_click(MENTAL_100)
-    if(ctypes.windll.user32.GetAsyncKeyState(0x32)&0x8000):
+    if(ctypes.windll.user32.GetAsyncKeyState(0x32)&ctypes.windll.user32.GetAsyncKeyState(0x12)&0x8000):
         perfect_click(MENTAL_90)
-    if(ctypes.windll.user32.GetAsyncKeyState(0x33)&0x8000):
+    if(ctypes.windll.user32.GetAsyncKeyState(0x33)&ctypes.windll.user32.GetAsyncKeyState(0x12)&0x8000):
         perfect_click(MENTAL_70)
-    if(ctypes.windll.user32.GetAsyncKeyState(0x34)&0x8000):
+    if(ctypes.windll.user32.GetAsyncKeyState(0x34)&ctypes.windll.user32.GetAsyncKeyState(0x12)&0x8000):
         perfect_click(MENTAL_50)
-    if(ctypes.windll.user32.GetAsyncKeyState(0x35)&0x8000):
+    if(ctypes.windll.user32.GetAsyncKeyState(0x35)&ctypes.windll.user32.GetAsyncKeyState(0x12)&0x8000):
         perfect_click(MENTAL_20)
+    if(ctypes.windll.user32.GetAsyncKeyState(0x51)&ctypes.windll.user32.GetAsyncKeyState(0x12)&0x8000):
+        pyautogui.press('backspace')
+        mental_input()
     if(ctypes.windll.user32.GetAsyncKeyState(0x1B)&0x8000):
         break
